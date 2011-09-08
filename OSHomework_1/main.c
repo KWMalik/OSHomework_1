12733a726e2fbbbf051efa80722a56e2a6ba19d4
Kurry Tran <kurry.tran@gmail.com>
2011-09-08 12:44:09 -0400

    Initial Commit

---
+++ OSHomework_1/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  OSHomework_1
+//
+//  Created by Kurry Tran on 9/8/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
